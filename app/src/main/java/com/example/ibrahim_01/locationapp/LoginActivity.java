a920e735448ea1469e18263a396788f0371de5f3
IbrahimAliAsghar <mysterious.ibrahim@gmail.com>
2017-07-13 15:45:20 +0500

    inserted private fields

--- app/src/main/java/com/example/ibrahim_01/locationapp/LoginActivity.java
+++ app/src/main/java/com/example/ibrahim_01/locationapp/LoginActivity.java
@@ -2,12 +2,34 @@ package com.example.ibrahim_01.locationapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private Button buttonRegister;
+    private EditText email;
+    private EditText password;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonLogin = (Button) findViewById(R.id.btnLogin);
+
+        buttonRegister = (Button) findViewById(R.id.btnRegister);
+
+        email = (EditText) findViewById(R.id.editTextEmail);
+
+        password = (EditText) findViewById(R.id.editTxtPassword);
+
+
+
+
+
+
     }
 }
