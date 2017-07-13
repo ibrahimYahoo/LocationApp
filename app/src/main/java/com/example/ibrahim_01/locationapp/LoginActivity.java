bb203b43a996ff085ed425ee6beda2c1e088b33a
IbrahimAliAsghar <mysterious.ibrahim@gmail.com>
2017-07-13 16:14:07 +0500

    implement login onclick

--- app/src/main/java/com/example/ibrahim_01/locationapp/LoginActivity.java
+++ app/src/main/java/com/example/ibrahim_01/locationapp/LoginActivity.java
@@ -2,11 +2,15 @@ package com.example.ibrahim_01.locationapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static  String TAG = "Logs";
 
     private Button buttonRegister;
     private EditText email;
@@ -20,16 +24,24 @@ public class LoginActivity extends AppCompatActivity {
 
         buttonLogin = (Button) findViewById(R.id.btnLogin);
 
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"yos kesa hae",Toast.LENGTH_LONG);
+                Log.d(TAG,"yos console pae agya");
+            }
+        });
+
         buttonRegister = (Button) findViewById(R.id.btnRegister);
 
         email = (EditText) findViewById(R.id.editTextEmail);
 
         password = (EditText) findViewById(R.id.editTxtPassword);
 
-
-
-
-
-
     }
+
+
+
+
+
 }
